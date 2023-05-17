1eb80b916d2e730f12fa941e6e63ac2328f6c929
swyftcs <nicholaszdrzalka@hotmail.com>
2023-05-16 19:09:01 -0500

    Updated files to display sites on ec2 ip and setup for chatbot implementation - Nick

--- resume_builder/resume_builder/settings.py
+++ resume_builder/resume_builder/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'password',
+        'PASSWORD': 'koteczek',
         'HOST': '0.0.0.0',
         'PORT': '5432',
     }
