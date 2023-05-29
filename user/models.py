379d1674725b3d044a6c87fe941f59e995c8ffac
yesiguava <yguevara693@gmail.com>
2023-05-28 22:25:42 -0500

    Setting Up User Sign In

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class User(models.Model):
+    email = models.CharField(max_length=50)
+    resume_info = models.CharField()
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, primary_key=False)
+
+    def __str__(self) -> str:
+        return self.email + " " + self.resume_info
